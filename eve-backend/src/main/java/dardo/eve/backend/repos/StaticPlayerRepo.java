162c2c99478f72a107e205b94eddb457888280c0
donkeystalk <donkeystalk@gmail.com>
2012-02-29 22:54:29 -0600

    updated repo

--- eve-backend/src/main/java/dardo/eve/backend/repos/StaticPlayerRepo.java
+++ eve-backend/src/main/java/dardo/eve/backend/repos/StaticPlayerRepo.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 public class StaticPlayerRepo implements PlayerRepo {
 
 	public String findPlayer() {
-		return "this is a player";
+		return " is a player";
 	}
 
 }
