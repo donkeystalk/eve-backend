0a18a48c93567b1f417ab99af3e2a84c4ecb347d
Jonathan Kupcho <donkeystalk@gmail.com>
2012-03-09 16:17:40 -0600

    Updated ItemResource

--- eve-backend/src/main/java/dardo/eve/api/ItemResource.java
+++ eve-backend/src/main/java/dardo/eve/api/ItemResource.java
@@ -1,28 +1,40 @@
 package dardo.eve.api;
 
-import java.util.Collections;
 import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import dardo.eve.backend.models.MarketCategory;
+import dardo.eve.backend.repos.CategoryRepo;
 
 @Path("/item")
 @Component
 public class ItemResource 
 {
 	
+	@Autowired
+	private CategoryRepo categoryRepo;
+	
 	@GET
-	@Path("/names")
+	@Path("/marketGroups")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<MarketCategory> findBaseMarketCategories()
 	{
-		return Collections.EMPTY_LIST;
+		return categoryRepo.findBaseMarketCategories();
+	}
+	
+	@GET
+	@Path("/marketGroups/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<MarketCategory> findMarketCategoriesByParentID(@PathVariable Integer id)
+	{
+		return categoryRepo.findMarketGroupByParentID(id);
 	}
 }
